ef141aa3d47fc9b70f12a842c573ca7965a6806c
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2025-01-22 00:12:16 +0500

    Add

--- components/elements/ThemeSwitch.tsx
+++ components/elements/ThemeSwitch.tsx
@@ -1,11 +1,13 @@
 'use client'
+
 import { useEffect, useState } from "react"
 
 export default function ThemeSwitch() {
-	const [theme, setTheme] = useState<string>("light")
+	const [theme, setTheme] = useState<string>("dark")
 
 	useEffect(() => {
-		// Access localStorage only on the client-side
+		// Default to "light" theme and then check localStorage for saved theme
+		document.documentElement.setAttribute("data-bs-theme", "light")
 		const savedTheme = localStorage?.getItem("theme") || "light"
 		setTheme(savedTheme)
 		document.documentElement.setAttribute("data-bs-theme", savedTheme)
@@ -22,16 +24,12 @@ export default function ThemeSwitch() {
 	}
 
 	return (
-		<>
-			<div
-				className="dark-light-switcher pe-10 pe-lg-0 pe-0 ps-md-5 ps-0 ps-lg-4 pe-lg-4 d-flex justify-content-center align-items-center icon_80"
-				onClick={toggleTheme}
-				style={{ cursor: "pointer" }}
-			>
-				<i className={`bi theme-icon ${theme === "light" ? "ri-sun-line text-warning" : "ri-contrast-2-line text-white"}`} />
-			</div>
-
-			
-		</>
+		<div
+			className="dark-light-switcher pe-10 pe-lg-0 pe-0 ps-md-5 ps-0 ps-lg-4 pe-lg-4 d-flex justify-content-center align-items-center icon_80"
+			onClick={toggleTheme}
+			style={{ cursor: "pointer" }}
+		>
+			<i className={`bi theme-icon ${theme === "light" ? "ri-sun-line text-warning" : "ri-contrast-2-line text-white"}`} />
+		</div>
 	)
 }
