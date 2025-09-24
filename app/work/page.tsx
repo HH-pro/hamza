0f0832acd99649f75fe8e04a063aa6b5a7fb5485
HH-pro <hamzamanzoor0011@gmail.com>
2025-09-25 01:05:48 +0500

    Auto update

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -187,11 +187,12 @@ export default function Work() {
 										<div className="card-custom" data-index={0}>
 											<div className="card__inner bg-6 p-lg-6 p-md-4 p-3">
 												<div className="card__image-container zoom-img position-relative">
-													<img 
-  className=" w-4 h-40 object-cover rounded-lg mx-auto" 
+<img 
+  className="card__image !w-40 !h-40 object-cover rounded-lg mx-auto" 
   src="/assets/imgs/work/plants.jpeg" 
   alt="Plant Caring App" 
 />
+
 
 												</div>
 												<div className="card__content px-md-4 px-3">
