1aa83528bc0fea7138d1d61d5b54ee952522bafe
HH-pro <hamzamanzoor0011@gmail.com>
2026-02-19 02:40:52 +0500

    Auto update

--- components/elements/PortfolioFilter.tsx
+++ components/elements/PortfolioFilter.tsx
@@ -258,7 +258,7 @@ export default function PortfolioFilter() {
 							<div className="text-center">
 								<img
 									className="rounded-3 zoom-img mx-auto"
-									src="/assets/imgs/projects/projects-1/facme.jpeg"
+									src="/assets/imgs/projects/projects-1/faceme.jpeg"
 									alt="FacmeQuiz iOS App"
 									style={{ width: "220px", height: "auto", maxWidth: "100%" }}
 								/>
