285e5928b4c6a53fd6c3f6e17832ba971fd86643
HH-pro <hamzamanzoor0011@gmail.com>
2026-02-19 03:17:34 +0500

    Auto update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import "/public/assets/css/vendors/swiper-bundle.min.css"
 import "/public/assets/css/vendors/carouselTicker.css"
 import "/public/assets/css/vendors/magnific-popup.css"
 import "/public/assets/fonts/remixicon/remixicon.css"
-import "/public/assets/css/main.css"
+import "../public/assets/css/main.css"
+
 
 import type { Metadata } from "next"
 import { Urbanist, Playfair_Display, DM_Mono } from "next/font/google"
