27e33fded7945ee5fd9e2bd747545de0398052e9
HH-pro <hamzamanzoor0011@gmail.com>
2026-02-19 02:30:06 +0500

    Auto update

--- components/sections/Projects1.tsx
+++ components/sections/Projects1.tsx
@@ -1,3 +1,4 @@
+
 import Link from "next/link"
 import dynamic from 'next/dynamic'
 const PortfolioFilter = dynamic(() => import('../elements/PortfolioFilter'), {
@@ -7,6 +8,7 @@ const PortfolioFilter = dynamic(() => import('../elements/PortfolioFilter'), {
 export default function Projects1() {
 	return (
 		<>
+
 			<div id="projects" className="section-projects-1 position-relative pt-120 pb-6 bg-900">
 				<div className="container">
 					<div className="row align-items-end">
@@ -19,8 +21,7 @@ export default function Projects1() {
 							</span>
 						</div>
 						<div className="col-lg-auto">
-							{/* Desktop button - visible on large screens */}
-							<Link href="/work" className="btn btn-gradient mt-lg-0 mt-5 ms-lg-auto d-none d-xl-inline-flex align-items-center gap-2">
+							<Link href="/work" className="btn btn-gradient mt-lg-0 mt-5 ms-lg-auto d-none d-xl-block">
 								View All Projects
 								<i className="ri-arrow-right-up-line" />
 							</Link>
@@ -28,20 +29,14 @@ export default function Projects1() {
 					</div>
 				</div>
 			</div>
-			
 			<div className="bg-900 fillter-project" data-background="assets/imgs/projects/projects-1/background.png">
 				<PortfolioFilter/>
 			</div>
-			
-			{/* Mobile/Tablet button - visible on smaller screens, hidden on xl */}
-			<div className="container overflow-hidden">
+			<div className="contairer overflow-hidden">
 				<div className="row justify-content-center position-relative button-project pb-160 bg-900 pt-1">
-					<Link 
-						href="/work" 
-						className="icon_hover position-relative z-1 icon-shape icon_150 border-linear-2 rounded-circle position-relative overflow-hidden bg-white hover-up d-xl-none"
-					>
+					<Link href="/work" className="icon_hover position-relative z-1 icon-shape icon_150 border-linear-2 rounded-circle position-relative overflow-hidden bg-white hover-up">
 						<span className="icon-shape icon-md bg-linear-2 rounded-circle position-absolute bottom-0 end-0" />
-						<p className="m-0 fs-7 fw-bold text-capitalize position-absolute top-50 start-50 translate-middle d-flex align-items-center gap-1">
+						<p className="m-0 fs-7 fw-bold text-capitalize position-absolute top-50 start-50 translate-middle">
 							View All
 							<i className="ri-arrow-right-up-line fs-7" />
 						</p>
@@ -49,6 +44,7 @@ export default function Projects1() {
 					<div className="ellipse position-absolute bottom-0 start-50 translate-middle-x z-0" />
 				</div>
 			</div>
+
 		</>
 	)
 }
