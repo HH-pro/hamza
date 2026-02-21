a4a7130e560c947952a2c7604fe24db4ffd04eca
HH-pro <hamzamanzoor0011@gmail.com>
2026-02-22 02:03:35 +0500

    Auto update

--- components/elements/PortfolioFilter.tsx
+++ components/elements/PortfolioFilter.tsx
@@ -231,7 +231,7 @@ export default function PortfolioFilter() {
 							<a href="https://bettolettings.co.uk/" target="_blank" rel="noopener noreferrer">
 								<img
 									className="rounded-3 w-100 zoom-img"
-									src="/assets/imgs/projects/projects-1/betto-lettings.jpg"
+									src="/assets/imgs/projects/projects-1/7.png"
 									alt="Betto Lettings"
 								/>
 							</a>
